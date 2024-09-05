377156b7aeef07f72e891fffbc2cc3b149cdfad8
Santiago Sixtos <menny_2208@hotmail.com>
2024-09-05 09:52:37 -0500

    05/09/2024

---
+++ Parcial1/RecepcionDeDatos/Index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require ('cors');
+
+app.use(cors()); //Middleware de tercero
+app.use(express.json()); //Middleware incorporado en express
+ 
+app.get('/clientes/id:',cors(),(req,res)=>{
+    console.log(req.params)
+    res.json({Mensaje: 'Server Express contestando a peticion get'})
+})
+ 
+app.post('/clientes',(req,res)=>{
+    console.log(req.query)
+    res.json({Mensaje: 'Server Express contestando a peticion post'})
+})
+
+app.put('/clientes',(req,res)=>{
+    console.log(req.body)
+    res.json({Mensaje: 'Server Express contestando a peticion put'})
+})
+ 
+app.listen(3001,()=>{
+    console.log('http://localhost:3000')
+})
+
