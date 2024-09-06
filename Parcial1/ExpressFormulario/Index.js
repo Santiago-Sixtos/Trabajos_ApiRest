19b978508f0611ac270aa70df636543b409585b7
Santiago Sixtos <menny_2208@hotmail.com>
2024-09-06 09:46:24 -0500

    06/09/2024

---
+++ Parcial1/ExpressFormulario/Index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require ('cors');
+const multer = require('multer')
+const bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+
+const app = express();
+const upload = multer();
+
+app.use(cors()); //Middleware de tercero
+app.use(express.json()); //Middleware incorporado en express
+app.use(express.text()); //Middleware de texto 
+app.use(express.urlencoded({extended: true})); //Middleware para parsear un formulario url encode
+app.use(upload.none())
+app.use(bodyParser.xml()); //Formularios xml
+
+app.post('/clientes',(req,res)=>{
+    console.log(req.body)
+    res.json({Mensaje: 'Server Express contestando a peticion post'})
+})
+ 
+app.listen(3001,()=>{
+    console.log('http://localhost:3000')
+})
+
