646caadf8c24d963bc24db5a6c3790101c420413
Santiago Sixtos <menny_2208@hotmail.com>
2024-09-12 09:41:03 -0500

    Actualizacion Post, Delete

--- Parcial1/ExpressBaseDeDatos/Index.js
+++ Parcial1/ExpressBaseDeDatos/Index.js
@@ -41,6 +41,46 @@ app.get('/tdg', (req, res) => {
     res.json(results);  // Devolver los resultados
   });
 });
+
+app.post('/tdg', (req, res) => {
+  const nuevoGatito = req.body; // Datos enviados por el cliente en el cuerpo de la solicitud
+
+  // Realiza las operaciones necesarias 
+  const insertQuery = "INSERT INTO tdg (ID, Nombre, Raza, Peso, Edad, Color) VALUES (?, ?, ?, ?, ?, ?)";
+  const values = [nuevoGatito.ID, nuevoGatito.Nombre, nuevoGatito.Raza, nuevoGatito.Peso, nuevoGatito.Edad, nuevoGatito.Color];
+
+  connection.query(insertQuery, values, (error, results) => {
+    if (error) {
+      console.error('Error al insertar el gatito:', error);
+      return res.status(500).json({ message: 'Error al insertar el gatito' });
+    }
+
+    console.log('Gatito insertado correctamente');
+    res.status(201).json({ message: 'Gatito insertado correctamente' });
+  });
+});
+
+
+app.delete('/tdg/:id', (req, res) => {
+  const gatitoId = req.params.id; // ID proporcionado en la URL
+
+  // Realiza la eliminación del gatito con el ID proporcionado
+  const deleteQuery = "DELETE FROM tdg WHERE ID = ?";
+  connection.query(deleteQuery, [gatitoId], (error, results) => {
+    if (error) {
+      console.error('Error al eliminar el gatito:', error);
+      return res.status(500).json({ message: 'Ocurrió un error en la eliminación' });
+    }
+
+    if (results.affectedRows === 1) {
+      console.log('Gatito borrado correctamente');
+      res.json({ message: 'Gatito borrado correctamente' });
+    } else {
+      res.json({ message: 'No se encontró ningún gatito con el ID especificado' });
+    }
+  });
+});
+
  
 // Escuchar en el puerto 3001
 app.listen(3001, () => {
