9f917c57fe6f2e9a18e5ba234a2c8b9702beec87
Santiago Sixtos <menny_2208@hotmail.com>
2024-10-16 09:13:02 -0500

    Validación

---
+++ Parcial2/ValidaExpress/Index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator')
+const app = express();
+ 
+const  cors  = require ('cors');
+app.use(cors());
+ 
+app.get('/usuario', check('edad').isNumeric(),(req,res)=>{
+    const result = validationResult(req);
+
+    if (result.isEmpty()) {
+        res.send({mensaje:'Server Express contestando a peticion get'});
+    } else {
+        res.json(result);    
+    } 
+})
+ 
+app.listen(3001,()=>{
+    console.log('http://localhost:3000')
+})
+
+//Implementar un validor de un objeto que reciba de mi api
