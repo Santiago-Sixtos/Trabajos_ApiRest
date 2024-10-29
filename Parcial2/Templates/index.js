ec12254cd0ae74c942a13b4a4b58dff5f11eed64
Santiago Sixtos <menny_2208@hotmail.com>
2024-10-29 12:41:03 -0500

    Template Engines

--- Parcial2/Templates/index.js
+++ Parcial2/Templates/index.js
@@ -1,19 +1,18 @@
-const express = requiere('express');
-const path = requiere('path');
-const pug = requiere('pug');
+const express = require('express');
+const pug = require('pug');
+const path = require('path');
+ 
 const app = express();
-const cors = requier('cors');
-
-app.use(cors())
+ 
 app.set('view engine', 'pug');
-app.set('views', path.join(_dirname, 'views'));
-
-app.get('/', (req, res, next) =>{
+app.set('views', path.join(__dirname, 'views'));
+ 
+app.get('/mivista', (req, res, next) => {
     console.log(req.query.nombre);
-    let opciones = { nombre:req.query.nombre };
-    res.render('mivista', opciones)
-})
-
-app.listen(3000, () => {
-    console.log('Server Express escuchando en el puerto 3000');
+    let opciones = { nombre: req.query.nombre };
+    res.render('index', opciones);
+});
+ 
+app.listen(3001, () => {
+    console.log('Server Express escuchando en puerto 3001');
 });
