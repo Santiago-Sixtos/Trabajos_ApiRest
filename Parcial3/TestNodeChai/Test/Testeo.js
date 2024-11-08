9a872daace0ca4d8417b8769a263036a3a100841
Santiago Sixtos <menny_2208@hotmail.com>
2024-11-07 19:23:05 -0600

    Prueba de testeo con chai

---
+++ Parcial3/TestNodeChai/Test/Testeo.js
@@ -0,0 +1,16 @@
+import assert from 'node:assert'; //Asegurar la respuesta
+import test from 'node:test'; // Importamos el Test de node para realizarlo en nuestro script
+import * as chai from 'Chai';
+import * as area from "../src/area.js";  //Importamos el metodo a realizar
+
+test("Deberia volver un 4",()=>{
+    let res = area.areaCuadrado(2);
+    //assert.strictEqual(res, 4);
+    chai.assert.equal(res, 4);
+    chai.assert.typeOf(res, 'number');
+})
+
+test("Deberia volver un 4",()=>{
+    let res = area.areaCuadrado(2);
+    chai.expect(res).to.be.a('number');
+})
